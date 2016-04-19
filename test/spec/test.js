07cb08c93adac5a4fa6feaea2071dee580e68f2e
Dane Petersen <dane@ge.com>
2016-04-19 15:14:39 -0700

    Add mocha and chai with some dummy tests

--- test/spec/test.js
+++ test/spec/test.js
@@ -1,10 +1,22 @@
 (function () {
   'use strict';
 
-  describe('Give it some context', function () {
-    describe('maybe a bit more context here', function () {
-      it('should run here few assertions', function () {
+  var foo = 'bar';
+  var beverages = { tea: [ 'chai', 'matcha', 'oolong' ] };
 
+  describe('Give it some context', () => {
+    describe('maybe a bit more context here', function () {
+      it('foo should be a string', () => {
+        foo.should.be.a('string');
+      });
+      it('foo should equal bar', () => {
+        foo.should.equal('bar');
+      });
+      it('foo should be 3 characters long', () => {
+        foo.should.have.length(3);
+      });
+      it('beverages should have tea and tea should be three', () => {
+        beverages.should.have.property('tea').with.length(3);
       });
     });
   });
