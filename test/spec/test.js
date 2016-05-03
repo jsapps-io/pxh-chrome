2356dc1a5e20dc1462f923136b475077032e0802
Dane Petersen <dane@ge.com>
2016-05-03 16:41:33 -0500

    Add tests for switching between states

--- test/spec/test.js
+++ test/spec/test.js
@@ -1,6 +1,45 @@
 'use strict';
 
 var pxhSubObject = pxhFindObjectByLabel(pxhStatesObject, 'drawerOpen');
+// create a state object for testing purposes
+var testStatesObject = {
+  'stateOne' : {
+    'qa-item-one' : {
+      'remove' : 'qa-item-one--state-two',
+      'add' : 'qa-item-one--state-one',
+    },
+    'qa-item-two' : {
+      'remove' : 'qa-item-two--state-two',
+      'add' : 'qa-item-two--state-one',
+    },
+  },
+  'stateTwo' : {
+    'qa-item-one' : {
+      'remove' : 'qa-item-one--state-one',
+      'add' : 'qa-item-one--state-two',
+    },
+    'qa-item-two' : {
+      'remove' : 'qa-item-two--state-one',
+      'add' : 'qa-item-two--state-two',
+    },
+  }
+}
+
+// assign individual test states from the test states object to variables
+var testStateOne = pxhFindObjectByLabel(testStatesObject, 'stateOne');
+var testStateTwo = pxhFindObjectByLabel(testStatesObject, 'stateTwo');
+
+// create item to manipulate by toggling state
+var stateItemOne = document.createElement('div');
+stateItemOne.classList.add('qa-item-one');
+stateItemOne.classList.add('qa-item-one--state-one');
+document.body.appendChild(stateItemOne);
+
+// create item to manipulate by toggling state
+var stateItemTwo = document.createElement('div');
+stateItemOne.classList.add('qa-item-two');
+stateItemOne.classList.add('qa-item-two--state-one');
+document.body.appendChild(stateItemTwo);
 
 // create element to test adding a single class
 var addClass = document.createElement('div');
@@ -45,6 +84,8 @@ pxhChangeClasses('qa-target-removed', 'remove', 'qa-remove');
 pxhChangeClasses('qa-target-removed-multiple', 'remove', 'qa-remove-multiple-one qa-remove-multiple-two');
 pxhChangeClasses('qa-target-toggle-multiple', 'toggle', 'qa-toggle-add-multiple-one qa-toggle-add-multiple-two qa-toggle-remove-multiple-one qa-toggle-remove-multiple-two');
 
+// fire the changes for pxhLoadState
+pxhLoadState(testStatesObject, 'stateTwo');
 
 describe('Tests for pxh-chrome.js', () => {
   describe('Tests for pxhFindObjectByLabel', () => {
@@ -76,6 +117,12 @@ describe('Tests for pxh-chrome.js', () => {
       assert.notDeepEqual(document.getElementsByClassName('qa-target-toggle-multiple'), document.getElementsByClassName('qa-toggle-remove-multiple-two'));
     });
   });
+  describe('Tests for pxhLoadState', () => {
+    it('pxhLoadState switches from stateOne to stateTwo', () => {
+      assert.deepEqual(document.getElementsByClassName('qa-item-one'), document.getElementsByClassName('qa-item-one--state-two'));
+      assert.deepEqual(document.getElementsByClassName('qa-item-two'), document.getElementsByClassName('qa-item-two--state-two'));
+      assert.notDeepEqual(document.getElementsByClassName('qa-item-one'), document.getElementsByClassName('qa-item-one--state-one'));
+      assert.notDeepEqual(document.getElementsByClassName('qa-item-two'), document.getElementsByClassName('qa-item-two--state-one'));
     });
   });
 });
