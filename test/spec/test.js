5f99536f5d1d4da72f93b8da1ac51761804a9cc0
Dane Petersen <dane@ge.com>
2016-05-03 09:39:27 -0500

    Use expect and comment out default tests

--- test/spec/test.js
+++ test/spec/test.js
@@ -1,22 +1,27 @@
 'use strict';
 
-  var foo = 'bar';
-  var beverages = { tea: [ 'chai', 'matcha', 'oolong' ] };
+// var foo = 'bar';
+// var beverages = { tea: [ 'chai', 'matcha', 'oolong' ] };
+var subObject = findObjectByLabel(statesObject, 'drawerOpen');
 
-  describe('Give it some context', () => {
-    describe('maybe a bit more context here', function () {
-      it('foo should be a string', () => {
-        foo.should.be.a('string');
-      });
-      it('foo should equal bar', () => {
-        foo.should.equal('bar');
-      });
-      it('foo should be 3 characters long', () => {
-        foo.should.have.length(3);
-      });
-      it('beverages should have tea and tea should be three', () => {
-        beverages.should.have.property('tea').with.length(3);
-      });
+describe('Give it some context', () => {
+  describe('maybe a bit more context here', function () {
+    it('we should be able to return an object by label from the state object', () => {
+      assert.isObject(subObject, 'subObject is an object');
     });
+    
+    // it('foo should be a string', () => {
+    //   assert.isString(foo, 'foo is a string');
+    // });
+    // it('foo should equal bar', () => {
+    //   assert.strictEqual(foo, 'bar', 'foo equals bar');
+    // });
+    // it('foo should be 3 characters long', () => {
+    //   assert.lengthOf(foo, 3, 'foo has length of 3');
+    // });
+    // it('beverages should have tea and tea should be three', () => {
+    //   assert.property(beverages, 'tea');
+    //   assert.lengthOf(beverages.tea, 3, 'beverages has 3 types of tea');
+    // });
   });
 });
