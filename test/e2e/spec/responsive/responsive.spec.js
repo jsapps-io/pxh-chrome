59a40ef3c3ddcec95efca4f4922c391dd030891f
Dane Petersen <dane@ge.com>
2016-05-06 10:49:39 -0500

    Add desktop e2e tests and improve mobile and tablet tests

--- test/e2e/spec/responsive/responsive.spec.js
+++ test/e2e/spec/responsive/responsive.spec.js
@@ -44,6 +44,10 @@ describe('pxh-chrome', function() {
       leftPosition = commonPage.convertPxToNum(leftPosition);
       assert.isAtLeast(leftPosition, 0);
     });
+    mainComponents.getDrawer().getCssValue('width').then(function (drawerWidth) {
+      drawerWidth = commonPage.convertPxToNum(drawerWidth);
+      assert.isAtLeast(drawerWidth, 200);
+    });
   });
 
   it('should close the drawer when the drawer toggle is clicked', function() {
@@ -60,6 +64,10 @@ describe('pxh-chrome', function() {
     browser.driver.manage().window().setSize(800, 800);
     browser.driver.sleep(1000);
     assert.eventually.isOk(mainComponents.getDrawer().isDisplayed());
+    mainComponents.getDrawer().getCssValue('left').then(function (leftPosition) {
+      leftPosition = commonPage.convertPxToNum(leftPosition);
+      assert.isAtLeast(leftPosition, 0);
+    });
     mainComponents.getDrawer().getCssValue('width').then(function (drawerWidth) {
       drawerWidth = commonPage.convertPxToNum(drawerWidth);
       assert.isAtMost(drawerWidth, 100);
@@ -83,46 +91,84 @@ describe('pxh-chrome', function() {
       leftPosition = commonPage.convertPxToNum(leftPosition);
       assert.isAtLeast(leftPosition, 0);
     });
+    mainComponents.getDrawer().getCssValue('width').then(function (drawerWidth) {
+      drawerWidth = commonPage.convertPxToNum(drawerWidth);
+      assert.isAtLeast(drawerWidth, 200);
+    });
   });
 
   it('should narrow the drawer when the view header drawer toggle is clicked', function() {
     browser.driver.manage().window().setSize(800, 800);
     mainComponents.clickDrawerToggle();
     browser.driver.sleep(1000);
-    mainComponents.getDrawer().getCssValue('width').then(function (drawerWidth) {
-      drawerWidth = commonPage.convertPxToNum(drawerWidth);
-      assert.isAtMost(drawerWidth, 100);
-    });
     mainComponents.getDrawer().getCssValue('left').then(function (leftPosition) {
       leftPosition = commonPage.convertPxToNum(leftPosition);
       assert.isAtLeast(leftPosition, 0);
     });
+    mainComponents.getDrawer().getCssValue('width').then(function (drawerWidth) {
+      drawerWidth = commonPage.convertPxToNum(drawerWidth);
+      assert.isAtMost(drawerWidth, 100);
+    });
   });
 
-  // it('at the desktop breakpoint and above, shows the wide drawer', function() {
-  //   browser.driver.manage().window().setSize(1200, 800);
-  //   browser.driver.sleep(1000);
-  //   assert.eventually.isOk(mainComponents.getDrawer());
-  //   assert.eventually.isOk(mainComponents.getDrawer().isDisplayed());
-  //   var drawerWidth = mainComponents.getDrawer().getCssValue('width');
-  //   drawerWidth = convertPxToNum(drawerWidth);
-  //   // assert.eventually.isAtLeast(Promise.resolve(drawerWidth), 200);
-  //   var leftPosition = mainComponents.getDrawer().getCssValue('left');
-  //   leftPosition = convertPxToNum(leftPosition);
-  //   // assert.eventually.isAtMost(Promise.resolve(drawerWidth), 0);
-  // });
+  it('should show the wide drawer at the desktop breakpoint', function() {
+    browser.driver.manage().window().setSize(1100, 800);
+    browser.driver.sleep(1000);
+    assert.eventually.isOk(mainComponents.getDrawer().isDisplayed());
+    mainComponents.getDrawer().getCssValue('left').then(function (leftPosition) {
+      leftPosition = commonPage.convertPxToNum(leftPosition);
+      assert.isAtLeast(leftPosition, 0);
+    });
+    mainComponents.getDrawer().getCssValue('width').then(function (drawerWidth) {
+      drawerWidth = commonPage.convertPxToNum(drawerWidth);
+      assert.isAtLeast(drawerWidth, 200);
+    });
+  });
 
+  it('should narrow the drawer when the view header drawer toggle is clicked', function() {
+    browser.driver.manage().window().setSize(1100, 800);
+    mainComponents.clickDrawerToggle();
+    browser.driver.sleep(1000);
+    mainComponents.getDrawer().getCssValue('left').then(function (leftPosition) {
+      leftPosition = commonPage.convertPxToNum(leftPosition);
+      assert.isAtLeast(leftPosition, 0);
+    });
+    mainComponents.getDrawer().getCssValue('width').then(function (drawerWidth) {
+      drawerWidth = commonPage.convertPxToNum(drawerWidth);
+      assert.isAtMost(drawerWidth, 100);
+    });
+  });
 
+  it('should expand the drawer when the view header drawer toggle is clicked', function() {
+    browser.driver.manage().window().setSize(1100, 800);
+    mainComponents.clickDrawerToggle();
+    browser.driver.sleep(1000);
+    mainComponents.getDrawer().getCssValue('left').then(function (leftPosition) {
+      leftPosition = commonPage.convertPxToNum(leftPosition);
+      assert.isAtLeast(leftPosition, 0);
+    });
+    mainComponents.getDrawer().getCssValue('width').then(function (drawerWidth) {
+      drawerWidth = commonPage.convertPxToNum(drawerWidth);
+      assert.isAtLeast(drawerWidth, 200);
+    });
+  });
 
-  // it('should change to tablet layout', function() {
-  //   browser.driver.manage().window().setSize(800, 800);
-  //   browser.driver.sleep(1000);
-  //   assert.eventually.isOk(mainComponents.getDrawer().isDisplayed());
-  //   var drawerWidth = mainComponents.getDrawer().getCssValue('width');
-  //   drawerWidth = convertPxToNum(drawerWidth);
-  //   // assert.eventually.isAtMost(Promise.resolve(drawerWidth), 100);
-  //   var leftValue = mainComponents.getDrawer().getCssValue('left');
-  //   leftValue = convertPxToNum(leftValue);
-  //   // assert.eventually.isAtMost(Promise.resolve(drawerWidth), 0);
-  // });
+  it('should remember that the drawer is narrow between refreshes', function() {
+    this.timeout(10000);
+    browser.driver.manage().window().setSize(1100, 800);
+    mainComponents.clickDrawerToggle();
+    browser.driver.sleep(1000);
+    browser.driver.navigate().refresh();
+    browser.driver.sleep(1000);
+    mainComponents.getDrawer().getCssValue('left').then(function (leftPosition) {
+      leftPosition = commonPage.convertPxToNum(leftPosition);
+      assert.isAtLeast(leftPosition, 0);
+    });
+    mainComponents.getDrawer().getCssValue('width').then(function (drawerWidth) {
+      drawerWidth = commonPage.convertPxToNum(drawerWidth);
+      assert.isAtMost(drawerWidth, 100);
+    });
+    browser.driver.sleep(1000);
+  });
+
 });
