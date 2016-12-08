cd90364e3528ca8ecf3104849c96940f8358e966
Pete Butler <pete.butler@ge.com>
2016-12-08 12:24:55 -0800

    flattening to dist/sass/

--- flatten.js
+++ flatten.js
@@ -4,9 +4,6 @@ let importResolve = require('import-resolve');
 
 importResolve({
     ext: "scss",
-    includePaths: ['.', 'bower_components'],
     pathToMain: 'sass/pxh-chrome.scss',
-    output: 'flattened/pxh-chrome.scss'
+    output: 'dist/sass/pxh-chrome.scss'
 });
-
-
